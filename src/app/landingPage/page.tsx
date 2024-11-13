cd2d7a5ae7a226eac50d62b77a6af8b6abcb24a0
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2024-11-13 02:13:02 -0800

    update

--- src/app/landingPage/page.tsx
+++ src/app/landingPage/page.tsx
@@ -1,9 +1,77 @@
 import React from "react";
 import { MoveRight } from "lucide-react";
 
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../../public/assets/logoo.png";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+
 export default function landingPage() {
   return (
     <div className="bg-[#F3F9FB]">
+      <nav className="sticky inset-x-0 top-0 z-30 w-5/6 m-auto border-b border-gray-200 backdrop-blur-lg transition-all py-4">
+          <div className="flex justify-between items-center w-full">
+            <Link href="/" className="z-40 flex gap-1 items-center">
+              <Image
+                src={logo}
+                alt="culturehub logo"
+                className="size-12 -rotate-3"
+              />
+              <span className="font-extrabold text-xl text-[#113F67]">culturehub.</span>
+            </Link>
+
+            {/* if sign out */}
+            <div className="flex items-center gap-4">
+              <SignedOut>
+                <SignInButton>
+                  <Button variant={"ghost"} size={"sm"}>
+                    Sign In
+                  </Button>
+                </SignInButton>
+                <SignUpButton>
+                  <Button
+                    className="bg-blue-600 text-white text-xs hover:bg-blue-500 transition-colors"
+                    size={"sm"}
+                  >
+                    Get Started
+                    <ArrowRight className="size-5" />
+                  </Button>
+                </SignUpButton>
+              </SignedOut>
+
+              <SignedIn>
+                <Link href="/feed">
+                  <Button variant={"ghost"} size={"md"} className='mx-1 p-2 px-3 hover:border-b-2 hover:border-[#113F67] hover:bg-[#F3F9FB]'>
+                    Feed
+                  </Button>
+                </Link>
+                <Link href="/dashboard">
+                  <Button variant={"ghost"} size={"md"} className='mx-1 p-2 px-3 hover:border-b-2 hover:border-[#113F67] hover:bg-[#F3F9FB]'>
+                    Dashboard
+                  </Button>
+                </Link>
+                {/* by nippun */}
+                <Link href="/landingPage">
+                  <Button variant={"ghost"} size={"md"} className='mx-1 p-2 px-3 hover:border-b-2 hover:border-[#113F67] hover:bg-[#F3F9FB] mr-6'>
+                    Landing
+                  </Button>
+                </Link>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </div>
+      </nav>
+
       <div className="w-2/3 m-auto">
         <section className="hero text-center py-28 flex flex-row content-center">
           <div className="w-1/2 self-center justify-center">
